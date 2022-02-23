7de4d89531e0892880f11ac009543ca8a5d52822
DanielBL01 <dblee0532@gmail.com>
2022-02-23 01:45:21 -0500

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,13 +21,38 @@ function Home() {
                 <a href="https://uwaterloo.ca/"> University of Waterloo</a>.
             </p>
             <p>
+                Feel free to take a look at my<a href="/resume.pdf" target="_blank"> resume</a>.
+            </p>
+            <h3>Past</h3>
+            <p>
                 Previously, I've interned as a Back End Developer at <a href="https://www.ibm.com/ca-en">IBM</a> and as a
                 Software Engineer at <a href="https://lumenix.com/">Lumenix</a>. Since then, I've had the opportunity to work
                 in different development environments, from writing Java code in large existing codebases to building web applications from 
                 the ground up to production.
             </p>
+            <h3>Present</h3>
             <p>
-                Feel free to take a look at my<a href="/resume.pdf" target="_blank"> resume</a>.
+                I am currently in my 3A term taking the following courses.
+            </p>
+            <p>
+                <li>Introduction to Thermodynamics and Heat Transfer</li>
+                <li>Microprocessor Systems and Interfacing</li>
+                <li>Kinematics and Dynamics of Machines</li>
+                <li>Actuators and Power Electronics</li>
+                <li>Systems Models</li>
+            </p>
+            <p>
+                Additionally, I am currently reading the following books/resources.
+            </p>
+            <p>
+            <li><a href="https://www.oreilly.com/library/view/head-first-design/9781492077992/">Head First Design Patterns, 2nd Edition</a> (Learn applicable design patterns for object-oriented software)</li>
+            <li><a href="https://www.oreilly.com/library/view/designing-data-intensive-applications/9781491903063/">Designing Data-Intensive Applications</a> (Learn data systems, databases, caches, indexes, distributed systems and more)</li>
+            <li><a href="https://www.educative.io/courses/grokking-the-system-design-interview">Grokking the System Design Interview</a> (Learn about designing systems at a high-level overview)</li>
+            </p>
+            <h3>What's next?</h3>
+            <p>
+                For the upcoming Summer 2022 term, I will be interning at <a href="https://www.pepperdata.com/">Pepperdata</a> as a Backend Developer. With this 
+                internship, I will be focusing on learning about Big Data and Distributed Systems!
             </p>
             <hr />
             <div className="socials">
