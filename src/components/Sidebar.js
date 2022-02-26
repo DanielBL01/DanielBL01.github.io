d72708e3424f8f58e4e3ea6b54a766a9094a99aa
DanielBL01 <dblee0532@gmail.com>
2022-02-26 02:43:44 -0500

    fixing some styling preferences

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,12 +4,12 @@ import { slide as Options } from 'react-burger-menu'
 function Sidebar() {
     return (
         <Options right>
-            <a className="menu-item" href="/">Home.</a>
-            <a className="menu-item" href="/resume">Resume.</a>
-            <a className="menu-item" href="/about">About.</a>
-            <a className="menu-item" href="/projects">Projects.</a>
-            <a className="menu-item" href="/experience">Experience.</a>
-            <a className="menu-item" href="/blog">Blog.</a>
+            <a className="sidebar-item" href="/">Home.</a>
+            <a className="sidebar-item" href="/resume">Resume.</a>
+            <a className="sidebar-item" href="/about">About.</a>
+            <a className="sidebar-item" href="/projects">Projects.</a>
+            <a className="sidebar-item" href="/experience">Experience.</a>
+            <a className="sidebar-item" href="/blog">Blog.</a>
       </Options>
     )
 }
