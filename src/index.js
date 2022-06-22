15e8cdfce82e71006ea2b4e08918d26d5941c18a
DanielBL01 <dblee0532@gmail.com>
2022-06-22 18:49:40 -0400

    attempt to fix https redirect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import HttpsRedirect from 'react-https-redirect';
 import './styles/App.css';
 
 ReactDOM.render(
-  <App />,
+  <HttpsRedirect>
+    <App />
+  </HttpsRedirect>,
   document.getElementById('root')
 );
